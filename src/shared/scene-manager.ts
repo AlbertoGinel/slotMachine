8a858cef6007afdc9bbda26a8bf933706185f104
Alberto Ginel <aginelsalvador@gmail.com>
2023-09-26 00:41:26 +0300

    fix for phone

--- src/shared/scene-manager.ts
+++ src/shared/scene-manager.ts
@@ -2,10 +2,7 @@ import { Application } from "pixi.js";
 import { Stage, Layer } from "@pixi/layers";
 import { diffuseGroup, normalGroup, lightGroup } from "@pixi/lights";
 
-
-
 export class SceneManager {
-    //class is almost will be static
     private constructor() { };
     private static _app: Application;
     private static _currentScene: IScene;
@@ -21,17 +18,18 @@ export class SceneManager {
     public static init(background: number): void {
         SceneManager._app = new Application({
             view: document.getElementById("pixi-screen") as HTMLCanvasElement,
-            resizeTo: window,
+            width: SceneManager.width,
+            height: SceneManager.height,
             resolution: window.devicePixelRatio || 1,
             autoDensity: true,
             backgroundColor: background,
         });
 
+        // The initial resize
+        SceneManager.resize();
 
         //Debug
         (globalThis as any).__PIXI_APP__ = SceneManager._app;
-
-
 
         SceneManager._app.stage = new Stage();
         SceneManager._app.stage.addChild(
@@ -47,9 +45,7 @@ export class SceneManager {
     public static changeScene(newScene: IScene): void {
         if (SceneManager._currentScene) {
             SceneManager._app.stage.removeChild(SceneManager._currentScene as any);
-            //SceneManager._currentScene.destroy();
         }
-
         // Add the new one
         SceneManager._currentScene = newScene;
         SceneManager._app.stage.addChild(SceneManager._currentScene as any);
@@ -62,16 +58,25 @@ export class SceneManager {
     }
 
     public static resize(): void {
+        const scaleFactor = Math.min(
+            window.innerWidth / SceneManager._app.screen.width,
+            window.innerHeight / SceneManager._app.screen.height
+        );
+
+        const newWidth = Math.ceil(SceneManager._app.screen.width * scaleFactor);
+        const newHeight = Math.ceil(SceneManager._app.screen.height * scaleFactor);
+
+        SceneManager._app.renderer.resize(newWidth, newHeight);
+        SceneManager._app.stage.scale.set(scaleFactor);
+
         // if we have a scene, we let it know that a resize happened!
         if (SceneManager._currentScene) {
-            SceneManager._currentScene.resize(SceneManager.width, SceneManager.height);
+            SceneManager._currentScene.resize(newWidth, newHeight);
         }
     }
 }
 
 export interface IScene {
-    //children: DisplayObject[];
     update(framesPassed: number): void;
-    // we added the resize method to the interface
     resize(screenWidth: number, screenHeight: number): void;
 }
